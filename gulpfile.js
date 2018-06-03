966714670caa29fbbe14e29c2d6ae5874725eae3
Chris <chris@line26.com>
2018-06-03 10:23:33 +1000

    quick commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var imageResize = require('gulp-image-resize');
 
 
 gulp.task('default', function () {
+
+    gulp.src('./img_raw/*/*.jpg')
+        .pipe(imageResize({
+            width : 600,
+            height : 900,
+            crop : true,
+            upscale : false
+        }))
+        .pipe(gulp.dest('./web/img'));
+
+
     return gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./web/css'));
